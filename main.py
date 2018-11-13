97e81d7ee67e1387be273332c4d3be3df314d456
Karm Desai <karmdesai18@gmail.com>
2018-11-12 20:00:06 -0500

    Debugging
    Fixed some minor errors, cleaned up the code so that it is more concise.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-#This is the accompanying module to 'db_connect.py'
 import sqlite3
-import time
 import sys
+import time
 
 conn = sqlite3.connect('info_base.db')
 cur = conn.cursor()
 
-#Use this to introduce the program and return the user's choice
+#Create the table
+cur.execute('CREATE TABLE IF NOT EXISTS userInfo (username, password, first name, last name, email, phone number, age, FaceBook, Twitter, GitHub)')
+conn.commit()
+
+#Welcome the user, find out what they what they want to do with info-base
 def welcome():
+    global choice
+
     print("Welcome to info-base, where you'll be able to store your information and later access it.\n")
     choice = input("If you'd like to create a new file, enter 'c'. If you'd like to view a file, enter 'v'.\n").lower()
 
@@ -19,11 +24,9 @@ def welcome():
     else:
         print("Ok, let's view an existing file!")
 
-    return choice
-
 #Use this function to collect information regarding a new user
-def create():
-    print("Ok, let's get started with your file. Answer the questions that come up as you would like them to appear on your file.\n")
+def collect():
+    print("Ok, let's get started with your file. Answer the questions that come up as you would like them to appear on your file. \nEnter 'N/A' if you don't want to answer the question.\n")
     userName = input("Choose a username for your log-in. Remember it - it cannot be reset!\n")
 
     cur.execute("SELECT rowid FROM userInfo WHERE username = (?)", (userName,))
@@ -78,6 +81,92 @@ def create():
     twitter = '@' + input("What is your Twitter username?\n")
     gitHub = '@' + input("What is your gitHub username?\n")
 
+    print("I've created your file. Thanks for using info-base!")
+
     return (userName, userPass, firstName, lastName, eMail, phoneNo, age, faceBook, twitter, gitHub)
+
+def gather ():
+    global userQuery, queryData
+
+    userQuery = input('Enter your username to find your data...\n')
+
+    cur.execute('SELECT rowid FROM userInfo WHERE username = (?)', (userQuery,))
+    data = cur.fetchall()
+
+    # Ensure that the user is registered with info-base
+    if len(data) == 0:
+        print(f'The username @{userQuery} does not exist in info-base!\n Goodbye!')
+        time.sleep(3)
+        sys.exit()
+
+    tryPass = input(f'Enter the password for the user: @{userQuery}...\n')
+
+    cur.execute('SELECT password FROM userInfo WHERE username = (?)', (userQuery,))
+    data = cur.fetchall()
+
+    while data != [(tryPass,)]:
+        print('Incorrect Password...\n')
+        tryPass = input(f'Enter the password for the user: @{userQuery}...\n')
+
+    cur.execute('''SELECT first name, last name, email, phone number, age, FaceBook, Twitter, GitHub 
+                FROM userInfo WHERE username = (?)''', (userQuery,))
+
+    queryData = cur.fetchall()
+
+#Create a user using the tuple returned by the ask.create() function
+def create_user():
+    cur.execute('INSERT INTO userInfo VALUES (?,?,?,?,?,?,?,?,?,?)', collect())
+    conn.commit()
+
+def show_data():
+    gather()
+    #Display all the user's data, line by line
+    print(f'Displaying your data, ...@{userQuery}\n')
+    time.sleep(1)
+
+    print('Your first name is: ', queryData[0][0])
+    print('Your last name is: ', queryData[0][1])
+    print('Your e-mail address is: ', queryData[0][2])
+    print('Your phone number is: ', queryData[0][3])
+    print(f'You are {queryData[0][4]} years old.')
+    print('Your faceBook username is: ', queryData[0][5])
+    print('Your Twitter username is: ', queryData[0][6])
+    print(f'Your gitHub username is: ', queryData[0][7])
+
+welcome()
+
+if choice == 'c':
+    create_user()
+
+elif choice == 'v':
+    show_data()
+
+else:
+    print('An unknown error occurred... \nAbort...')
+    time.sleep(3)
+    sys.exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
